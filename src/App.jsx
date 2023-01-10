dc75505210fe0c4197536fbb67f07a65dc264b5e
Sevindzh Shukurova <shukurova.s.n@gmail.com>
2023-01-10 16:08:50 -0500

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import MiNavbar from './components/MiNavbar';
+import './App.css';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 const App = () => {
   return (
-    <h1>ecomm-manor</h1>
+    <BrowserRouter>
+   <MiNavbar />
+   </BrowserRouter>
   )
 }
 
